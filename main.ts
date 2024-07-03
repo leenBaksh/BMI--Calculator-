ddab86cb878675ee3abfe55d15c45f0f4ae99c1e
leenBaksh <sandleenbakshi@gmail.com>
2024-07-03 22:25:20 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+// Prompt the user for weight and height
+const answer = await inquirer.prompt([
+  {
+    message: "Enter your weight in kg:",
+    type: "number",
+    name: "weightInKg",
+  },
+  {
+    message: "Enter your height in meters:",
+    type: "number",
+    name: "heightInM",
+  },
+]);
+
+// Calculate BMI
+let bmi = answer.weightInKg / (answer.heightInM * answer.heightInM);
+
+// Print the BMI
+console.log(chalk.magentaBright(`Your BMI is ${bmi.toFixed(2)}`));
